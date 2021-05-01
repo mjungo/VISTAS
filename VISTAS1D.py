feat: static (cw) calculation, fixes #3
fixes #3

- VISTASmodels.py: added function with system of nonlinear equations to be solved
- VISTAS1D.py: added iterative resolution of static characteristic (LI) using previous solution as initialisation for next current step. First initialisation calculated analytically (no stimulated emission below threshold)

VISTAS1D.py
@@ -40,7 +40,7 @@ from VCSEL_params import params
 
 def main():
     
-    # 1. key parameters --------------------------------------------------------------------------------------------  
+    # 1. key parameters --------------------------------------------------------------------------------
  
     (wl0, Rt, Rb, alpha_int, beta, Gamma, \
             tau_N, tau_esc , tau_cap, eta_i, rs, DN, \
@@ -63,10 +63,10 @@ def main():
     phi = phi[:, np.newaxis]    # np rank one array -> (1, nphi) vector
 
  
-    # 2. normalized modes intensity profiles -----------------------------------------------------------------------
+    # 2. normalized modes intensity profiles -----------------------------------------------------------
 
     # mode amplitude profiles
-    nm, lvec, LPlm, ur = LP_modes(wl0*1e-9, nc, delta_n, r_act*1e-2, r_cav*1e-2, nrho, rho.T*1e-2, alpha = 10)
+    nm, lvec, LPlm, ur = LP_modes(wl0 * 1e-9, nc, delta_n, r_act * 1e-2, r_cav * 1e-2, nrho, rho.T * 1e-2, alpha = 10)
 
     # normalized intensity profiles
     Ur = np.square(ur)  # field amplitude -> intensity
@@ -76,7 +76,7 @@ def main():
 
     print()
     if str(input('Plot 2D mode profiles (y/n)? ')) == 'y':
-        plot2D(Ur, LPlm, lvec, nm, rho, nrho, phi, nphi, nfig=1)
+        plot2D(Ur, LPlm, lvec, nm, rho, nrho, phi, nphi, nfig = 1)
 
     # size correction of parameters defined individually for each mode
     Rt = Rt[0:nm, :]
@@ -87,7 +87,7 @@ def main():
     Gamma = Gamma[0:nm, :]
 
     
-    # 3. radial injection current profile --------------------------------------------------------------------------
+    # 3. radial injection current profile --------------------------------------------------------------
 
     iact = (rho <= r_act) * 1   # "active" area, typically delimited by the oxide aperture
     ispread = (rho > r_act) * np.exp(-(rho - r_act) / rs)
@@ -98,10 +98,10 @@ def main():
     # plt.show()
     
     
-    # 4. current and time characteristics --------------------------------------------------------------------------
+    # 4. current and time characteristics --------------------------------------------------------------
 
     print()
-    modformat = int(input('Step (0), pulse (1), or large signal (2): '))
+    modformat = int(input('Step (0), single pulse (1), or large signal (2): '))
     tmax = float(input('  simulated time (ns): ')) * 1e-9
     tspan = [0, tmax]
     dt = 10e-12     # "reporting" timesteps (at which the solve_ivp is interpolated)
@@ -111,52 +111,52 @@ def main():
     ct=len(teval)
     ctFD=len(tevalFD)
 
-    it = np.ones((ct))      # used with solve_ivp solver (t_eval with typically 10ps timesteps)
+    It = np.ones((ct))      # used with solve_ivp solver (t_eval with typically 10ps timesteps)
     itFD = np.ones((ctFD))  # used with Finite Differences (FD) solver (t_eval with typically 1ps timesteps)
 
     if modformat == 0:
-        iOn = float(input('  DC current (mA): ')) * 1e-3
-        it = it * iOn
-        itFD = itFD * iOn
+        Ion = float(input('  DC current (mA): ')) * 1e-3
+        It = It * Ion
+        itFD = itFD * Ion
 
     elif modformat == 1:
-        iOn = float(input('  "on" current (mA): ')) * 1e-3
-        iOff = float(input('  "off" current (mA): ')) * 1e-3
-        it = it * iOn
-        it[int(ct/2):] = iOff
-        itFD = itFD * iOn
-        itFD[int(ctFD/2):] = iOff
+        Ion = float(input('  "on" current (mA): ')) * 1e-3
+        Ioff = float(input('  "off" current (mA): ')) * 1e-3
+        It = It * Ion
+        It[int(ct/2):] = Ioff
+        itFD = itFD * Ion
+        itFD[int(ctFD/2):] = Ioff
 
     elif modformat == 2:
-        iOn = float(input('  "on" current (mA): ')) * 1e-3
-        iOff = float(input('  "off" current (mA): ')) * 1e-3
+        Ion = float(input('  "on" current (mA): ')) * 1e-3
+        Ioff = float(input('  "off" current (mA): ')) * 1e-3
         tb=float(input('  single bit duration (ns): ')) * 1e-9
         nb = int(tmax / tb)    # number of bits
         sequence = np.random.randint(2, size = nb)
         cb = int(tb / dt)                # number of time steps for one bit
         cbFD = int(tb / dtFD)
-        it = it * iOn
-        itFD = itFD * iOn
+        It = It * Ion
+        itFD = itFD * Ion
         for i in range(nb):
-            it[i * cb : (i + 1) * cb] = it[i * cb : (i + 1) * cb] - (iOn - iOff) * sequence[i]
-            itFD[i * cbFD : (i + 1) * cbFD] = itFD[i * cbFD : (i + 1) * cbFD] - (iOn - iOff) * sequence[i]
+            It[i * cb : (i + 1) * cb] = It[i * cb : (i + 1) * cb] - (Ion - Ioff) * sequence[i]
+            itFD[i * cbFD : (i + 1) * cbFD] = itFD[i * cbFD : (i + 1) * cbFD] - (Ion - Ioff) * sequence[i]
 
 
-    # 5. coefficient arrays ----------------------------------------------------------------------------------------
+    # 5. coefficient arrays ----------------------------------------------------------------------------
 
     print()
     ni = int(input('Radial resolution (typically 7-20 terms): '))
 
     # carrier radial series expansion terms J0i = J0(gammai * rho / r_cav)
     gammai = np.asarray(ss.jn_zeros(1, ni-1))   # scipy function that calculates the roots of the Bessel function
-    gammai = np.insert(gammai, 0, 0, axis=0)    # prepend 0 root, not included by jn_zero
+    gammai = np.insert(gammai, 0, 0, axis = 0)    # prepend 0 root, not included by jn_zero
     gammai = gammai[:, np.newaxis]
     
     J0i = ss.jn(0, gammai * rho.T / r_cav)    # bessel exansion terms J0i (ni of them)
 
     # coefficients array for calculating the average carrier density Na over the active area Na
     c_act = np.zeros((1, ni))
-    c_act = 2 / r_act**2 * np.trapz(J0i[:,0:round(nrho*r_act/r_cav)].T * rho[0:round(nrho*r_act/r_cav), :], rho[0:round(nrho*r_act/r_cav), :], axis = 0)    # integrates in the active area only
+    c_act = 2 / r_act**2 * np.trapz(J0i[:, 0:round(nrho * r_act / r_cav)].T * rho[0:round(nrho * r_act / r_cav), :], rho[0:round(nrho * r_act / r_cav), :], axis = 0)    # integrates in the active area only
     c_act = c_act[:, np.newaxis].T  # (1, ni)
 
     # coefficients array that captures the spatial overlap between current and carrier profiles
@@ -178,7 +178,7 @@ def main():
     for m in range(nm):
         for i in range(ni):
             for j in range(ni):
-                    prodTmp = J0i[j, :] * Ur[m,:] * J0i[i,:] * rho.reshape((-1,)) # reshape -> dim (nrho, )
+                    prodTmp = J0i[j, :] * Ur[m, :] * J0i[i, :] * rho.reshape((-1,)) # reshape -> dim (nrho, )
                     prodTmp = prodTmp[:, np.newaxis]
                     c_st[m, i, j] = 2 * vg / (r_cav * ss.jn(0, gammai[i]))**2 * np.trapz(prodTmp, rho, axis = 0)    
 
@@ -193,14 +193,34 @@ def main():
     S2P = V_cav * hvl / wl0 * eta_opt * c_sp / Gamma * 1e3  # convert photon density to optical power (nm, 1)
 
 
-    # 6a.solution of system of ODEs using 'solve_idp' --------------------------------------------------------------
+    # 6.static solution --------------------------------------------------------------------------------
+
+    Imax = 10e-3                                # current range for LI characteristic
+    dI = 0.2e-3                                 # current steps (must be small enough to ensure convergence)
+    Icw = np.arange(0, Imax, dI)                  # current vector
+    NScw = np.zeros((ni + nm, Icw.shape[0]))    # NS solution matrix (ni+nm, Imax / dI)
+    NScw[0:ni, 1] = (c_inj * Icw[1] / (c_nst + c_diff)).reshape((-1,))              # N-analytical solution (no Rst) for 2nd current step
+    NScw[ni:ni + nm, 1] = (Gamma * beta * c_nst * NScw[0, 1] / c_sp).reshape((-1,)) # S-analytical solution (no Rst) for 2nd current step
+    
+    tcwSolverStart = time.time()    
+    for i in range(2, Icw.shape[0], 1): # sol@0: 0, sol@1: below-threshold analytical solution calculated above
+        args = (ni, nm, c_act, c_inj, Icw[i], c_diff, gln, c_st, Ntr, epsilon, Gamma, beta, c_nst, c_sp)
+        NScw[:, i] = fsolve(VISTASmodels.cw_1D, NScw[:, i - 1], args = args)
+    tcwSolverEnd = time.time()
+
+    print()
+    print(f'LI calculation: {np.round(tcwSolverEnd - tcwSolverStart, 3)}s')
+    plotPower(Icw * 1e3, NScw[ni:,:], S2P, LPlm, xlabel = 'current (mA)')
+
+
+    # 7a.solution of system of ODEs using 'solve_ivp' --------------------------------------------------
 
     NS = np.zeros((ni + nm, 1))
     NSinit = np.zeros((ni + nm))
 
-    it = interp1d(x = teval, y = it, fill_value = "extrapolate") # current changes over time and must match the solve_ivp integration points
+    It = interp1d(x = teval, y = It, fill_value = "extrapolate") # current changes over time and must match the solve_ivp integration points
 
-    args = (ni, nm, c_act, c_inj, it, c_diff, gln, c_st, Ntr, epsilon, Gamma, beta, c_nst, c_sp)
+    args = (ni, nm, c_act, c_inj, It, c_diff, gln, c_st, Ntr, epsilon, Gamma, beta, c_nst, c_sp)
 
     tSolverStart = time.time()
     sol = solve_ivp(VISTASmodels.solver_1D, (0, tmax), NSinit, t_eval = teval, method = 'RK23', dense_output = True, vectorized = True, args = args)
@@ -208,43 +228,42 @@ def main():
 
     print()
     print(f'Solve_ivp main loop: {np.round(tSolverEnd - tSolverStart, 3)}s')
-    plotPower(sol.t * 1e9, sol.y[ni:ni+nm], S2P, LPlm)
+    plotPower(sol.t * 1e9, sol.y[ni:ni+nm], S2P, LPlm, xlabel = 'time (ns)')
 
 
-    # 6b. solution of system of ODEs using finite differences -------------------------------------------------------
+    # 7b. solution of system of ODEs using finite differences ------------------------------------------
 
-    NFD = np.zeros((ni, ctFD))  # N0, N1, ..., NnN
-    Na = np.zeros((1, ctFD))    # average carrier density across the active area
-    SFD = np.zeros((nm, ctFD))  # multimode photon number matrix
+    # NFD = np.zeros((ni, ctFD))  # N0, N1, ..., NnN
+    # Na = np.zeros((1, ctFD))    # average carrier density across the active area
+    # SFD = np.zeros((nm, ctFD))  # multimode photon number matrix
 
-    Nto = np.zeros((ni, 1))
-    Sto = np.zeros((nm, 1))
+    # Nto = np.zeros((ni, 1))
+    # Sto = np.zeros((nm, 1))
     
-    tFDStart = time.time()
+    # tFDStart = time.time()
     
-    for ti in range(ctFD - 1):
-        Nto[:, 0] = NFD[:, ti]
-        Sto[:, 0] = SFD[:, ti]
+    # for ti in range(ctFD - 1):
+    #     Nto[:, 0] = NFD[:, ti]
+    #     Sto[:, 0] = SFD[:, ti]
 
-        dNdt, dSdt = VISTASmodels.FD_1D(ti, Nto, Sto, ni, nm, c_act, c_inj, itFD[ti], c_diff, gln, c_st, Ntr, epsilon, Gamma, beta, c_nst, c_sp) # rhs of system of ODEs
+    #     dNdt, dSdt = VISTASmodels.FD_1D(Nto, Sto, ni, nm, c_act, c_inj, itFD[ti], c_diff, gln, c_st, Ntr, epsilon, Gamma, beta, c_nst, c_sp) # rhs of system of ODEs
 
-        Ntn = Nto + dtFD * dNdt             # Finite Differences step
-        NFD[:, ti + 1] = Ntn.reshape((-1,))
+    #     Ntn = Nto + dtFD * dNdt             # Finite Differences step
+    #     NFD[:, ti + 1] = Ntn.reshape((-1,))
         
-        Stn = Sto + dtFD * dSdt             # Finite Differences step
-        SFD[:, ti + 1] = Stn.reshape((-1,))
+    #     Stn = Sto + dtFD * dSdt             # Finite Differences step
+    #     SFD[:, ti + 1] = Stn.reshape((-1,))
 
-    tFDEnd = time.time()
+    # tFDEnd = time.time()
     
-    # subsample (smaller dtFD needed to ensure convergence, but too dense for plotting)
+    # subsample (smaller dtFD needed to ensure convergence, but too dense for post-processing)
     NFD = NFD[:, ::int(dt/dtFD)]        
     SFD = SFD[:, ::int(dt/dtFD)]
     tevalFD = tevalFD[::int(dt/dtFD)]   # tevalFD = teval
 
     print()
     print(f'FD solution main loop: {np.round(tFDEnd - tFDStart, 3)}s')
-    plotPower(tevalFD * 1e9, SFD, S2P, LPlm)
-
+    plotPower(tevalFD * 1e9, SFD, S2P, LPlm, xlabel = 'time (ns)')
 
 
 def plot2D(Ur, LPlm, lvec, nm, rho, nrho, phi, nphi, nfig):
@@ -280,14 +299,14 @@ def plot2D(Ur, LPlm, lvec, nm, rho, nrho, phi, nphi, nfig):
         plt.show()
 
 
-def plotPower(t, S, S2P, modes):
+def plotPower(x, S, S2P, modes, xlabel):
     P = S * S2P
-    plt.plot(t, P.T)
+    plt.plot(x, P.T)
     plt.legend(modes)
-    plt.plot(t, np.sum(P, 0), 'k--')
+    plt.plot(x, np.sum(P, 0), 'k--')
     plt.xlim(xmin=0)
     plt.ylim(ymin=0)
-    plt.xlabel('time (ns)')
+    plt.xlabel(xlabel)
     plt.ylabel('optical output power (mW)')
     plt.grid()
     plt.show()

VISTASmodels.py
@@ -24,24 +24,51 @@
 
 import numpy as np
 
-# rhs of system of ODEs fed into solve_ivp solver ---------------------------------------------------
+# system of equations to be solved for calculation of LI characteristic
+def cw_1D(NS, ni, nm, c_act, c_inj, It, c_diff, gln, c_st, Ntr, epsilon, Gamma, beta, c_nst, c_sp): 
+    
+    N = NS[0 : ni]
+    N = N[:, np.newaxis]
+    S = NS[ni : ni + nm]
+    S = S[:, np.newaxis]
+    
+    #g0 = 5e-16
+    Na = np.matmul(c_act, N)                                # average carrier density over the active area
+    g0 = gln * np.log((Na + 1) / Ntr) / (Na-Ntr)            # fitted time domain logarithmic gain factor
 
-def solver_1D(ti, NS, ni, nm, c_act, c_inj, it, c_diff, gln, c_st, Ntr, epsilon, Gamma, beta, c_nst, c_sp): 
+    Inj = c_inj * It                                        # current injection term
+    Diff = c_diff * N                                       # carrier diffusion term
+    Rst = g0 * (np.squeeze(np.matmul(c_st, N)) \
+          - c_st[:, :, 0] * Ntr) * S / (1 + epsilon * S)    # stimulated recombination term
+    Rnst = c_nst * N                                        # non-stimulated photon recombination
+    Rsp = c_sp * S                                          # spontaneous photon recombination
+
+    Nsol = Inj - Rnst - Diff - np.sum(Rst, 0)[:, np.newaxis]
+    Ssol = -Rsp + Gamma * beta * Rnst[0, :] + Gamma * Rst[:, 0][:, np.newaxis]
+    NSsol = np.concatenate([Nsol, Ssol])
+    NSsol = NSsol.reshape((-1,))
+
+    return NSsol
+
+
+# system of ODEs to be solved for dynamic response calculation using solve_ivp solver
+def solver_1D(ti, NS, ni, nm, c_act, c_inj, It, c_diff, gln, c_st, Ntr, epsilon, Gamma, beta, c_nst, c_sp): 
     
     N = NS[0 : ni, 0]
-    N=N[:, np.newaxis]
+    N = N[:, np.newaxis]
     S = NS[ni : ni + nm, 0]
-    S=S[:, np.newaxis]
+    S = S[:, np.newaxis]
     
-    Na = np.matmul(c_act, N)                        # average carrier density over the active area
-    g0 = gln * np.log((Na + 1) / Ntr) / (Na-Ntr)    # fitted time domain logarithmic gain factor
+    #g0 = 5e-16
+    Na = np.matmul(c_act, N)                                # average carrier density over the active area
+    g0 = gln * np.log((Na + 1) / Ntr) / (Na-Ntr)            # fitted time domain logarithmic gain factor
 
-    Inj = c_inj * it(ti)                                        # current injection
-    Diff = c_diff * N                                           # carrier diffusion
+    Inj = c_inj * It(ti)                                    # current injection
+    Diff = c_diff * N                                       # carrier diffusion
     Rst = g0 * (np.squeeze(np.matmul(c_st, N)) \
-          - c_st[:, :, 0] * Ntr) * S / (1 + epsilon * S)        # stimulated recombination
-    Rnst = c_nst * N                                            # non-stimulated photon recombination
-    Rsp = c_sp * S                                              # spontaneous photon recombination
+          - c_st[:, :, 0] * Ntr) * S / (1 + epsilon * S)    # stimulated recombination
+    Rnst = c_nst * N                                        # non-stimulated photon recombination
+    Rsp = c_sp * S                                          # spontaneous photon recombination
 
     dNdt = Inj - Rnst - Diff - np.sum(Rst, 0)[:, np.newaxis]
     dSdt = -Rsp + Gamma * beta * Rnst[0, :] + Gamma * Rst[:, 0][:, np.newaxis]
@@ -50,43 +77,22 @@ def solver_1D(ti, NS, ni, nm, c_act, c_inj, it, c_diff, gln, c_st, Ntr, epsilon,
     return dNSdt
 
 
-# rhs of system of ODEs fed into finite differences algorithm ---------------------------------------
-def FD_1D(ti, Nto, Sto, ni, nm, c_act, c_inj, it, c_diff, gln, c_st, Ntr, epsilon, Gamma, beta, c_nst, c_sp): 
+# system of ODEs to be solved for dynamic response calculation using finite differences
+def FD_1D(Nto, Sto, ni, nm, c_act, c_inj, It, c_diff, gln, c_st, Ntr, epsilon, Gamma, beta, c_nst, c_sp): 
     
-    Na = np.matmul(c_act, Nto)                      # average carrier density over the active area
-    g0 = gln * np.log((Na + 1) / Ntr) / (Na-Ntr)    # fitted time domain logarithmic gain factor
+    #g0 = 5e-16
+    Na = np.matmul(c_act, Nto)                              # average carrier density over the active area
+    g0 = gln * np.log((Na + 1) / Ntr) / (Na-Ntr)            # fitted time domain logarithmic gain factor
 
-    Inj = c_inj * it                                            # current injection
-    Diff = c_diff * Nto                                         # carrier diffusion
+    Inj = c_inj * It                                        # current injection
+    Diff = c_diff * Nto                                     # carrier diffusion
     Rst = g0 * (np.squeeze(np.matmul(c_st, Nto)) \
-          - c_st[:, :, 0] * Ntr) * Sto / (1 + epsilon * Sto)    # stimulated recombination
-    Rnst = c_nst * Nto                                          # non-stimulated photon recombination
-    Rsp = c_sp * Sto                                            # spontaneous photon recombination
+         - c_st[:, :, 0] * Ntr) * Sto / (1 + epsilon * Sto) # stimulated recombination
+    Rnst = c_nst * Nto                                      # non-stimulated photon recombination
+    Rsp = c_sp * Sto                                        # spontaneous photon recombination
+    Rsp = c_sp * S                                          # spontaneous photon recombination
 
     dNdt = Inj - Rnst - Diff - np.sum(Rst, 0)[:, np.newaxis]
     dSdt = -Rsp + Gamma * beta * Rnst[0, :] + Gamma * Rst[:, 0][:, np.newaxis]
     
     return dNdt, dSdt
-
-
-# def solver_1D_nvect(ti, NS, ni, nm, c_inj, it, c_diff, g0, c_st, Ntr, epsilon, Gamma, beta, tau_N, tau_S): 
-#     # non-vectorized implementation    
-#     NS=NS.reshape((-1,))
-#     N = NS[0 : ni]
-#     N=N[:, np.newaxis]
-#     S = NS[ni : ni + nm]
-#     S=S[:, np.newaxis]
-    
-#     Inj = c_inj * it                                    # current injection term
-#     Diff = c_diff * N                                   # carrier diffusion term
-#     Rst = g0 * (np.squeeze(np.matmul(c_st, N)) \
-#         - c_st[:, 0] * Ntr) * S / (1 + epsilon * S)     # stimulated recombination term
-#     Rnst = N / tau_N                                    # non-stimulated photon recombination term
-#     Rsp = S / tau_S                                     # spontaneous photon recombination term
-
-#     dNdt = Inj - Rnst - Diff - np.sum(Rst, 0)[:, np.newaxis]
-#     dSdt = -Rsp + Gamma * beta * Rnst[0, :] + Gamma * Rst[:, 0][:, np.newaxis]
-#     dNSdt = np.concatenate([dNdt, dSdt])
-#     dNSdt = dNSdt.reshape((-1,))
-
-#     return dNSdt